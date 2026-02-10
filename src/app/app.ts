64c900255ff8745eb67e6b82a61826aa46e2dd74
alejandro1223342 <alejomuoss@gmail.com>
2026-02-10 13:05:04 -0500

    Practica proyecto Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('aprender-angular');
+  protected readonly title = signal('Primer proyecto Angular');
 }
