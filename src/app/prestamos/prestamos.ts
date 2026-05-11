afcd9dbe91cc3d0152bfa7329c97640328b04ad1
alejandro1223342 <alejomuoss@gmail.com>
2026-05-11 15:26:43 -0500

    feat/practica-bucle-for

--- src/app/prestamos/prestamos.ts
+++ src/app/prestamos/prestamos.ts
@@ -9,6 +9,11 @@ import { Component } from '@angular/core';
 export class Prestamos {
   web = "https://google.com";
   redirigir = false;
+  prestamosList = [
+    { id: 1, monto: 5500, estado: "pendiente" },
+    { id: 2, monto: 10000, estado: "pagado" },
+    { id: 3, monto: 7000, estado: "pendiente" }
+  ]
 
   cambiarRedirigir() {
     if (this.redirigir == false) {
