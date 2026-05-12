8896b96d798669d729b0d9d4b5605c7b115bd446
alejandro1223342 <alejomuoss@gmail.com>
2026-05-12 18:30:24 -0500

    feat/formunlarios-con-binding-y-NgStyle

--- src/app/components/cine/cine.ts
+++ src/app/components/cine/cine.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Pelicula } from '../../models/pelicula';
 import { FormsModule } from '@angular/forms';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-cine',
-  imports: [FormsModule],
+  imports: [FormsModule, NgStyle],
   templateUrl: './cine.html',
   styleUrl: './cine.css',
 })
@@ -19,6 +20,8 @@ export class Cine {
   public mi_pelicula:string = "";
 
   public peliculasSinDatos: string[] = [];
+
+  public color:string = "#FFFFFF";
 
   constructor(){
     this.titulo = "Peliculas";
