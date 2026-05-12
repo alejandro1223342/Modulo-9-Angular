a135309ac287f44bb9df9ce41b9e97cf171cce79
alejandro1223342 <alejomuoss@gmail.com>
2026-05-12 15:24:53 -0500

    feat/directivas-en-angular-NgStyle

--- src/app/ventas/ventas.ts
+++ src/app/ventas/ventas.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { Pelicula } from '../pelicula/pelicula';
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
-  imports: [Pelicula, NgClass],
+  imports: [Pelicula, NgClass,NgStyle],
   templateUrl: './ventas.html',
   styleUrl: './ventas.css',
 })
 export class Ventas {
   public colorear: boolean = false;
-  public mostrar: string = "no";
+  public mostrar: string = "si";
+
+  misEstilos= {
+    'background-color': 'blue',
+    'padding': '10px',
+    'font-weight': 'bold',
+    'font-style': 'italic',
+    'font-family': 'Courier New, Courier, monospace'
+  };
 }
